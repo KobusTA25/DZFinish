14e9aa2a41f1c55c0106347c52bf0e20181dc747
Tatiana25 <Tanya375293763203@outlook.com>
2023-04-17 16:53:24 +0300

    Создаем новый массив

--- DZFinish/Program.cs
+++ DZFinish/Program.cs
@@ -11,20 +11,8 @@
             Console.Write($"Элемент " + (i + 1) + ": ");
             array[i] = Console.ReadLine();
         }
-
-        string[] newArray = new string[array.Length];
+string[] newArray = new string[array.Length];
         int count = 0;
 
-        for (int i = 0; i < array.Length; i++)
-        {
-            if (array[i].Length <= 3)
-            {
-                newArray[count] = array[i];
-                count++;
-            }
-        }
-
-        Array.Resize(ref newArray, count);
-
-        Console.WriteLine("Первоначальный массив: [" + string.Join(", ", array) + "]");
-        Console.WriteLine("Новый массив: [" + string.Join(", ", newArray) + "]");
+  
+        
