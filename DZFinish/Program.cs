db45394dcafe277ebeadcee51474d4192402cf9e
Tatiana25 <Tanya375293763203@outlook.com>
2023-04-17 16:54:55 +0300

    проходимся по всем элементам массива, проверяем количество символов меньше или равно трем, если да то записываем элемент в новый массив

--- DZFinish/Program.cs
+++ DZFinish/Program.cs
@@ -14,5 +14,14 @@
 string[] newArray = new string[array.Length];
         int count = 0;
 
-  
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                newArray[count] = array[i];
+                count++;
+            }
+        }
+
+        
         
