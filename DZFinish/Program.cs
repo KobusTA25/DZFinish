a7f8b57f462c9178033bcc2c6e4a50b72e5a1968
Tatiana25 <Tanya375293763203@outlook.com>
2023-04-17 16:52:35 +0300

    Создаем исходный массив и заполняет его с клавиатуры

---
+++ DZFinish/Program.cs
@@ -0,0 +1,30 @@
+﻿
+        Console.Write("Введите длинну массива: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string[] array = new string[n];
+
+        Console.WriteLine($"Введите элементы массива:");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Элемент " + (i + 1) + ": ");
+            array[i] = Console.ReadLine();
+        }
+
+        string[] newArray = new string[array.Length];
+        int count = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                newArray[count] = array[i];
+                count++;
+            }
+        }
+
+        Array.Resize(ref newArray, count);
+
+        Console.WriteLine("Первоначальный массив: [" + string.Join(", ", array) + "]");
+        Console.WriteLine("Новый массив: [" + string.Join(", ", newArray) + "]");
